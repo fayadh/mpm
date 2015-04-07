247c892e0a50d0eb6e1c7a4051c06d21b3f6a512
fayadh <fayadh.almosawi@gmail.com>
2015-04-07 16:56:37 -0400

    Added scales.rb

---
+++ scales.rb
@@ -0,0 +1,18 @@
+class Scales
+
+attr_accessor :A, :As, :B, :C, :Cs, :D, :Ds, :E, :F, :Fs, :G, :Gs 
+
+def initialize(key, type)			#Scales come in keys and types.
+	key = @key 
+	type = @type
+end
+
+def retrieve
+end
+
+def db
+		
+end
+
+end
+
